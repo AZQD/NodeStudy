b7000deb9ee05c699d25c12f3b8a2e271252f4b1
AZQD <604972395@qq.com>
2019-06-25 17:39:44 +0800

    module

--- module/index.js
+++ module/index.js
@@ -1,6 +1,6 @@
 var fs = require('fs'); // 核心模块
 console.log(fs);
 
-var moduleObj = require('./1.module1.js'); // 文件模块
+var moduleObj = require('./module'); // 文件模块
 console.log(moduleObj);
 console.log(typeof moduleObj);
