01490d3835bd484cd1675d6d8342c85edbb2fd2b
AZQD <604972395@qq.com>
2019-06-25 21:26:01 +0800

    buffer

---
+++ buffer/index.js
@@ -0,0 +1,24 @@
+console.log(typeof Buffer);
+var str = "Hello 尚硅谷";
+let buf = Buffer.from(str);
+console.log(buf);
+console.log(buf.length);
+console.log(buf[0]);
+console.log(typeof buf);
+console.log(buf.toString());
+
+console.log('---------------');
+
+// var buf2 = new Buffer(10);
+// console.log(buf2);
+//
+let buf2 = Buffer.alloc(10);
+console.log(buf2);
+buf2[0] = 0xaa;
+console.log(buf2);
+console.log(buf2[0].toString(16));
+
+console.log('---------------');
+
+let buf3 = Buffer.allocUnsafe(10);
+console.log(buf3);
