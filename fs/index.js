403c3d34fcf6e2ff3e8743e3da66073979213bfb
AZQD <604972395@qq.com>
2019-06-27 16:45:00 +0800

    fs

--- fs/index.js
+++ fs/index.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
 
-// 打开文件
-fs.open('./test.txt', 'r', function (err, res) {
-  console.log(err, res);
-});
+// 同步方式打开文件
+// let fd = fs.openSync('./demo.txt', 'a');
+//
+// fs.writeSync(fd, '书山有路勤为径，学海无涯苦作舟。', 1, 1);
+//
+// fs.closeSync(fd);
+
+// 异步方式打开文件
+// fs.open('./demo.txt', 'w+', function (err, res) {
+//   if(!err){ // 错误优先
+//     console.log(res); // res：文件的描述符
+//   }
+// });
