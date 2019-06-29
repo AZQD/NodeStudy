f5ff69727e5b241ab15cf1e573140f6d58fdca39
AZQD <604972395@qq.com>
2019-06-30 01:46:23 +0800

    设置cookie

--- http/step0/index.js
+++ http/step0/index.js
@@ -3,7 +3,9 @@ var http = require('http');
 // http入门
 http.createServer(function (req, res) {
   res.setHeader('Content-type', 'text/html; charset=utf-8');
-  res.writeHead(200, 'Success');
+  res.writeHead(200, 'success', {
+    'Set-Cookie': ['name=liming', 'age=20'] // 设置cookie
+  });
   res.write('<h1>hello北京<h1>');
   res.end();
 }).listen(8080);
