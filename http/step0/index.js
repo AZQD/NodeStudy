47b1b393ce9171510dfe6eb19c9030e0d3dc3ae6
AZQD <604972395@qq.com>
2019-06-30 02:13:00 +0800

    writeHead

--- http/step0/index.js
+++ http/step0/index.js
@@ -2,8 +2,8 @@ var http = require('http');
 
 // http入门
 http.createServer(function (req, res) {
-  res.setHeader('Content-type', 'text/html; charset=utf-8');
   res.writeHead(200, 'success', {
+    'Content-type': 'text/html; charset=utf-8',
     'Set-Cookie': ['name=liming', 'age=20'] // 设置cookie
   });
   res.write('<h1>hello北京<h1>');
